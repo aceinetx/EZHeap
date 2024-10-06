678157158a226180031b6faf40149f545c1bcf37
aceinetx <158546973+aceinetx@users.noreply.github.com>
2024-10-06 14:27:01 +0300

    Initial commit

---
+++ src/test.c
@@ -0,0 +1,17 @@
+#include <ezheap.h>
+#include <stdio.h>
+
+int main()
+{
+  ezheap_init();
+
+  int *x = new (int);
+  *x = 123;
+  printf("%d\n", *x);
+
+  int *y = new (int);
+  *y = 69;
+  printf("%d\n", *y);
+
+  ezheap_destruct();
+}
