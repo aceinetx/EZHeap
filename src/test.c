32c68dfd5550775ec40845ff19b1ff133a7eeb01
aceinetx <mouselin82@gmail.com>
2025-04-12 19:19:54 +0300

    Formatting

--- src/test.c
+++ src/test.c
@@ -1,17 +1,16 @@
 #include <ezheap.h>
 #include <stdio.h>
 
-int main()
-{
-  ezheap_init();
+int main() {
+	ezheap_init();
 
-  int *x = new(int);
-  *x = 123;
-  printf("%d\n", *x);
+	int* x = new (int);
+	*x = 123;
+	printf("%d\n", *x);
 
-  int *y = new(int);
-  *y = 69;
-  printf("%d\n", *y);
+	int* y = new (int);
+	*y = 69;
+	printf("%d\n", *y);
 
-  ezheap_destruct();
+	ezheap_destruct();
 }
